fe68c7261a2e2eb1c3594d4098dd2d69dc2092d1
Jorge Jimenez <calenocam24@gmail.com>
2020-06-06 12:28:36 -0400

    create selector to interact with eventslisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+//Creating Unique Event Listeners for each element
+
+const nav = document.querySelectorAll(".nav a");
+
+const busImg = document.querySelector(".intro img");
+
+const introTitle = document.querySelector(".intro h2");
+
+const textTitle = document.querySelector(".content-section h2");
+
+const imgContent = document.querySelector(".content-section img");
+
+const texTitleRev = document.querySelector(".inverse-content h2");
+
+const imgContentRev = document.querySelector(".inverse-content img");
+
+const contentPick = document.querySelectorAll(".content-pick h4");
+
+const bottomSign = document.querySelectorAll(".btn");
+
+const footer = document.querySelector("footer p");
+
+
+
