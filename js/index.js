33f8e04fd1a185453f4f4270216f43dca1a3135b
Jorge Jimenez <calenocam24@gmail.com>
2020-06-06 18:06:54 -0400

    finished with doing 10 events

--- js/index.js
+++ js/index.js
@@ -1,25 +1,81 @@
 // Your code goes here
 //Creating Unique Event Listeners for each element
 
-const nav = document.querySelectorAll(".nav a");
+
+document.querySelectorAll("nav a").forEach((navbars) => {
+//mouseover
+    navbars.addEventListener("mouseover", () => {
+        console.log("mouse went over");
+        navbars.style.color = "green";
+    });
+//mouseout
+    navbars.addEventListener("mouseout", () => {
+        console.log("mouse went out");
+        navbars.style.color = "black";
+    });
+});
 
 const busImg = document.querySelector(".intro img");
+//mousemove
+busImg.addEventListener("mousemove", () => {
+    console.log("opacity image getting down, and smaller");
+    busImg.style.opacity = "70%";
+    busImg.style.width = "80%";
+});
+
+//mouseup
+busImg.addEventListener("mouseup", () => {
+    console.log("mouse going up, and size image getting big");
+    busImg.style.width = "150%";
+    busImg.style.opacity = "100%";
+});
 
 const introTitle = document.querySelector(".intro h2");
-
-const textTitle = document.querySelector(".content-section h2");
+//click
+introTitle.addEventListener("click", () => {
+    console.log("first");
+    introTitle.style.color = "red";
+});
 
 const imgContent = document.querySelector(".content-section img");
+//dblclick
+imgContent.addEventListener("dblclick",() => {
+    imgContent.style.transform = "scale(1.3)";
+});
 
-const texTitleRev = document.querySelector(".inverse-content h2");
 
 const imgContentRev = document.querySelector(".inverse-content img");
+//drag
+imgContentRev.addEventListener("drag", () => {
+    imgContentRev.style =  "border-radius: 50%; border: 2px solid red";
+});
+imgContentRev.addEventListener("dragend", () => {
+    imgContentRev.style = "border-radius: 1%;"
+});
 
-const contentPick = document.querySelectorAll(".content-pick h4");
+document.querySelectorAll(".btn").forEach((buttons) => {
+//resize
+    window.addEventListener("resize", () => {
+    console.log("it will resize buttons");
+    buttons.style = "color: red; background-color: yellow";
+    });
+});
+ 
+const container = document.querySelector("body");
+//keydown
+window.addEventListener("keydown", (e) => {
+    if (e.key === 'i') {
+    container.style = "color: blue; background-color: black";
+    }
+    else if ( e.key === "p") {
+    container.style  = "color: blacki; background-color: white";
+    };
+});
 
-const bottomSign = document.querySelectorAll(".btn");
-
-const footer = document.querySelector("footer p");
+//load
+// window.addEventListener("load", () => {
+//     alert("I am Learning a Lot!!!");
+// });
 
 
 
